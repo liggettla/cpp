cd3e23575c55819fb387a224158ef465c7fbffb9
liggettla <alexander.liggett@gmail.com>
2015-05-20 20:54:57 -0600

    Allow Moves

--- ch10_checkers.cpp
+++ ch10_checkers.cpp
@@ -8,10 +8,10 @@ void printBoard(string pieces[8][8])
         cout << "  _ _ _ _ _ _ _ _\n";
         cout << "8|" << pieces[0][7] << " " << pieces[1][7] << " " << pieces[2][7] << " " << pieces[3][7] << " " << pieces[4][7] << " " << pieces[5][7] << " " << pieces[6][7] << " " << pieces[7][7] << "|\n";
         cout << "7|" << pieces[0][6] << " " << pieces[1][6] << " " << pieces[2][6] << " " << pieces[3][6] << " " << pieces[4][6] << " " << pieces[5][6] << " " << pieces[6][6] << " " << pieces[7][6] <<    "|\n";
-        cout << "1|" << pieces[0][5] << " " << pieces[1][5] << " " << pieces[2][5] << " " << pieces[3][5] << " " << pieces[4][5] << " " << pieces[5][5] << " " << pieces[6][5] << " " << pieces[7][5] <<    "|\n";
-        cout << "1|" << pieces[0][4] << " " << pieces[1][4] << " " << pieces[2][4] << " " << pieces[3][4] << " " << pieces[4][4] << " " << pieces[5][4] << " " << pieces[6][4] << " " << pieces[7][4] <<    "|\n";
-        cout << "1|" << pieces[0][3] << " " << pieces[1][3] << " " << pieces[2][3] << " " << pieces[3][3] << " " << pieces[4][3] << " " << pieces[5][3] << " " << pieces[6][3] << " " << pieces[7][3] <<    "|\n";
-        cout << "1|" << pieces[0][2] << " " << pieces[1][2] << " " << pieces[2][2] << " " << pieces[3][2] << " " << pieces[4][2] << " " << pieces[5][2] << " " << pieces[6][2] << " " << pieces[7][2] <<    "|\n";
+        cout << "6|" << pieces[0][5] << " " << pieces[1][5] << " " << pieces[2][5] << " " << pieces[3][5] << " " << pieces[4][5] << " " << pieces[5][5] << " " << pieces[6][5] << " " << pieces[7][5] <<    "|\n";
+        cout << "5|" << pieces[0][4] << " " << pieces[1][4] << " " << pieces[2][4] << " " << pieces[3][4] << " " << pieces[4][4] << " " << pieces[5][4] << " " << pieces[6][4] << " " << pieces[7][4] <<    "|\n";
+        cout << "4|" << pieces[0][3] << " " << pieces[1][3] << " " << pieces[2][3] << " " << pieces[3][3] << " " << pieces[4][3] << " " << pieces[5][3] << " " << pieces[6][3] << " " << pieces[7][3] <<    "|\n";
+        cout << "3|" << pieces[0][2] << " " << pieces[1][2] << " " << pieces[2][2] << " " << pieces[3][2] << " " << pieces[4][2] << " " << pieces[5][2] << " " << pieces[6][2] << " " << pieces[7][2] <<    "|\n";
         cout << "2|" << pieces[0][1] << " " << pieces[1][1] << " " << pieces[2][1] << " " << pieces[3][1] << " " << pieces[4][1] << " " << pieces[5][1] << " " << pieces[6][1] << " " << pieces[7][1] <<    "|\n";
         cout << "1|" << pieces[0][0] << " " << pieces[1][0] << " " << pieces[2][0] << " " << pieces[3][0] << " " << pieces[4][0] << " " << pieces[5][0] << " " << pieces[6][0] << " " << pieces[7][0] <<    "|\n";
         cout << " |_ _ _ _ _ _ _ _\n";
@@ -23,21 +23,49 @@ void initializeBoard(string pieces[8][8])
     for(int x=0; x<8; x++)
     {
         cout << x << '\n';
-        for(int y=0; y<2; y++)
+        for(int y=0; y<3; y++)
         {
-            pieces[x][y] = "R";
+//            if(x%2==0 & y%2==1)
+ //           {
+                pieces[x][y] = "R";
+ //           }
         }
 
-        for(int y=6; y<8; y++)
+        for(int y=5; y<8; y++)
         {
             pieces[x][y] = "B";
         }
 
-        for(int y=2; y<6; y++)
+        for(int y=3; y<5; y++)
         {
             pieces[x][y] = " ";
         }
     }
+}
+
+void makeaMove(string pieces[8][8])
+{
+    int oldx;
+    int oldy;
+    int newx;
+    int newy;
+    cout << "Pick a Piece to Move: \n" << "x-Coord: \n";
+    cin >> oldx;
+    oldx = oldx - 1;
+    cout << "y-Coord: \n";
+    cin >> oldy;
+    oldy = oldy - 1;
+    cout << "Pick a Destination Square \n" << "x-Coord: \n";
+    cin >> newx;
+    newx = newx - 1;
+    cout << "y-Coord: \n";
+    cin >> newy;
+    newy = newy - 1;
+
+    pieces[oldx][oldy] = " ";
+    pieces[newx][newy] = "R";
+
+    printBoard(pieces);
 }
 
 int main()
@@ -45,4 +73,5 @@ int main()
     string pieces[8][8];
     initializeBoard(pieces);
     printBoard(pieces);
+    makeaMove(pieces);
 }
